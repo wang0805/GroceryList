5ab556c01db834d7d4dbdb51d1894bd0f860e28d
wang0805 <wh_wang@live.com>
2018-11-18 00:15:29 +0800

    Oauth

---
+++ src/components/activity.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Activity extends Component {
+  render() {
+    return <div>testing activity</div>;
+  }
+}
+
+export default withRouter(Activity);
